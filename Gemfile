b0eff591d9735c4c92c5e5271fe965769a9c7367
Paul Cowan <paul.cowan@bjss.com>
2019-07-26 17:21:06 +0100

    all data

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem 'sportdb-import', '~> 0.2.2'
 gem 'activerecord', '~> 5.2', '>= 5.2.3'
 gem 'sportdb-models', '~> 1.18'
 gem 'datafile', '~> 0.2.5'
--- build.rb
+++ build.rb
@@ -1,5 +1,9 @@
 require 'sportdb/import'
 
+## note: requires a local copy of the football.db clubs datasets
+##          see https://github.com/openfootball/clubs
+SportDb::Import.config.clubs_dir = './clubs'
+
 DB_CONFIG = {
   adapter:  'postgresql',
   encoding: 'unicode',
@@ -13,13 +17,15 @@ DB_CONFIG = {
 
 ActiveRecord::Base.establish_connection( DB_CONFIG )
 
-Dir.chdir "eng-england"
+## build database schema / tables
+SportDb.create_all
 
- ## build schema 
- SportDb.create_all 
+## turn on logging to console
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 
- ## turn on logging to console 
- ActiveRecord::Base.logger = Logger.new(STDOUT) 
 
- pack = CsvMatchImporter.new( '../england' ) 
- pack.import_leagues 
+## note: requires a local copy of the football.csv england datasets
+##          see https://github.com/footballcsv/england
+pack = CsvMatchImporter.new( './england' )
+pack.import_leagues
+
